494151e6498b9b6335c07e4f8663c3d3aeed582e
your-name <your-email-address>
2022-02-08 15:24:45 +0300

    first commit

---
+++ dict.py
@@ -0,0 +1,9 @@
+purse = dict()
+purse['money'] = 12
+purse['candy'] = 3
+purse['tissues'] = 27
+purse['perform'] = 21
+print(purse)
+print(purse['candy'])
+purse['candy'] = purse['candy'] + 4
+print(purse)
