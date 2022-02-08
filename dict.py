45d74bd3829e96f0ed5913c19e87483e1c00d80c
your-name <your-email-address>
2022-02-08 16:31:25 +0300

    counting with a dictionary

--- dict.py
+++ dict.py
@@ -1,3 +1,4 @@
+'''
 purse = dict()
 purse['money'] = 12
 purse['candy'] = 3
@@ -7,3 +8,11 @@ print(purse)
 print(purse['candy'])
 purse['candy'] = purse['candy'] + 4
 print(purse)
+'''
+#counting with a dictionary
+ccc = dict()
+ccc['csev'] = 1
+ccc['cwen'] = 1
+print(ccc)
+ccc['cwen'] = ccc['cwen'] + 1
+print(ccc)
