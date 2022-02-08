07f76f4151f39c675b00c26273f7b2c17955365e
your-name <your-email-address>
2022-02-08 19:23:54 +0300

    count with get()

--- dict.py
+++ dict.py
@@ -10,9 +10,25 @@ purse['candy'] = purse['candy'] + 4
 print(purse)
 '''
 #counting with a dictionary
+'''
 ccc = dict()
 ccc['csev'] = 1
 ccc['cwen'] = 1
 print(ccc)
 ccc['cwen'] = ccc['cwen'] + 1
 print(ccc)
+'''
+counts = dict()
+names = ['cwen', 'csev', 'zqian', 'csev', 'cwen']
+for name in names :
+    if name not in counts:
+        counts[name] = 1
+    else :
+        counts[name] = counts[name] + 1
+print(counts)
+#count with get()
+counts = dict()
+names = ['cwen', 'csev', 'zqian', 'csev', 'cwen']
+for name in names :
+    counts[name] = counts.get(name, 0) + 1
+print(counts)
